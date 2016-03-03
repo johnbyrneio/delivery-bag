39f4d58aaa3b9ed0d2cf884f649bea8ce6ac4a04
John Byrne <johnb@chef.io>
2016-03-02 21:08:01 -0500

    Fix documentation

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description      'Delivery build_cookbook for your data bags!'
 
 version          '1.0.0'
 
-source_url       'https://github.com/chef-cookbooks/delivery-truck'
-issues_url       'https://github.com/chef-cookbooks/delivery-truck/issues'
+source_url       'https://github.com/johnbyrneio/delivery-bag'
+issues_url       'https://github.com/johnbyrneio/delivery-bag/issues'
 
 supports 'ubuntu', '>= 12.04'
 supports 'redhat', '>= 6.5'
